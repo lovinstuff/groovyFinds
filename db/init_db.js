f233f5f248cca60d756dad9bfd671e76d581f303
Gabrielle La Madrid <galamadrid917@gmail.com>
2021-11-17 20:27:39 -0800

    FilterPage2.0

--- db/init_db.js
+++ db/init_db.js
@@ -27,6 +27,7 @@ async function buildTables() {
         id SERIAL PRIMARY KEY,
         name varchar(255) UNIQUE NOT NULL, 
         artist varchar(255) NOT NULL, 
+        genre varchar(255) NOT NULL, 
         image_url varchar(255), 
         price integer
       );
@@ -72,6 +73,7 @@ async function populateInitialData() {
       {
         name: "What's Going On",
         artist: "Marvin Gaye",
+        genre: "Motown",
         image_url:
           "https://upload.wikimedia.org/wikipedia/en/8/84/MarvinGayeWhat%27sGoingOnalbumcover.jpg",
         price: 70,
@@ -79,6 +81,7 @@ async function populateInitialData() {
       {
         name: "Pet Sounds",
         artist: "The Beach Boys",
+        genre: "Rock",
         image_url:
           "https://i.scdn.co/image/ab67616d0000b27365eb1ad8d8a037029ad41e4a",
         price: 100,
@@ -86,6 +89,7 @@ async function populateInitialData() {
       {
         name: "Blue",
         artist: "Joni Mitchell",
+        genre: "Folk",
         image_url:
           "https://upload.wikimedia.org/wikipedia/en/e/e1/Bluealbumcover.jpg",
         price: 95,
@@ -93,6 +97,7 @@ async function populateInitialData() {
       {
         name: "Permanent Waves",
         artist: "Rush",
+        genre: "Rock",
         image_url:
           "https://upload.wikimedia.org/wikipedia/en/5/51/Rush_Permanent_Waves.jpg",
         price: 200,
@@ -100,6 +105,7 @@ async function populateInitialData() {
       {
         name: "Love Yourself: Tear",
         artist: "BTS",
+        genre: "K-Pop",
         image_url:
           "https://upload.wikimedia.org/wikipedia/en/thumb/8/88/Love_Yourself_Tear_Cover.jpeg/220px-Love_Yourself_Tear_Cover.jpeg",
         price: 50,
@@ -107,6 +113,7 @@ async function populateInitialData() {
       {
         name: "Thriller",
         artist: "Michael Jackson",
+        genre: "Disco",
         image_url:
           "https://images-na.ssl-images-amazon.com/images/I/712aTlKjhqL._SL1500_.jpg",
         price: 500,
@@ -114,6 +121,7 @@ async function populateInitialData() {
       {
         name: "Back in Black",
         artist: "AC/DC",
+        genre: "Rock",
         image_url:
           "https://upload.wikimedia.org/wikipedia/commons/b/be/Acdc_backinblack_cover.jpg",
         price: 400,
@@ -121,6 +129,7 @@ async function populateInitialData() {
       {
         name: "The Dark Side of the Moon",
         artist: "Pink Floyd",
+        genre: "Rock",
         image_url:
           "https://images-na.ssl-images-amazon.com/images/I/61R7gJadP7L._SX355_.jpg",
         price: 450,
@@ -128,6 +137,7 @@ async function populateInitialData() {
       {
         name: "The Bodyguard",
         artist: "Whitney Houston",
+        genre: "Pop", 
         image_url:
           "https://lastfm.freetls.fastly.net/i/u/770x0/8eed8ec437944c00cc8c4a306283de9b.jpg",
         price: 350,
@@ -135,6 +145,7 @@ async function populateInitialData() {
       {
         name: "Bat Out of Hell",
         artist: "Meat Loaf",
+        genre: "Rock", 
         image_url:
           "https://usercontent.one/wp/www.alltopeverything.com/wp-content/uploads/2020/05/Meat-Loaf-Bat-out-Of-Hell-600x600.jpg?media=1633479099",
         price: 300,
